ca243398cc66ad560cc0ac43033d9cd259cfd9ac
RobWHickman <robwhickman@gmail.com>
2025-11-12 12:35:19 +0000

    round out pkce initialisation

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,10 @@
-use fpl_rs::pkce::{pkce_challenge, pkce_verifier};
+use fpl_rs::pkce::pkce_init;
+use reqwest::blocking::Client;
 
 fn main() {
-    let verifier = pkce_verifier();
-    let challenge = pkce_challenge(&verifier);
+    let client = Client::new();
+    let (verifier, challenge, state) = pkce_init();
+    
 
     println!("all done!")
 }
--- src/pkce.rs
+++ src/pkce.rs
@@ -1,17 +1,25 @@
 use data_encoding::BASE64URL_NOPAD;
 use getrandom::fill;
 use sha2::{Digest, Sha256};
+use uuid::Uuid;
 
-pub fn pkce_verifier() -> String {
+fn pkce_verifier() -> String {
     let mut bytes = [0u8; 96];
     fill(&mut bytes).unwrap();
     BASE64URL_NOPAD.encode(&bytes)
 }
 
-pub fn pkce_challenge(verifier: &str) -> String {
+fn pkce_challenge(verifier: &str) -> String {
     let mut hasher = Sha256::new();
     hasher.update(verifier.as_bytes());
     let digest = hasher.finalize();
 
     BASE64URL_NOPAD.encode(&digest)
+}
+
+pub fn pkce_init() ->(String, String, String) {
+    let state = Uuid::new_v4().simple().to_string();
+    let verifier = pkce_verifier();
+    let challenge = pkce_challenge(&verifier);
+    (verifier, challenge, state)
 }
